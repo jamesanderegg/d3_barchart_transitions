6ead7586aa21f346a48f746a02fea04f1fe99d71
Juicyjames <iceandjames@gmail.com>
2020-02-03 12:54:16 -0700

    xScale and yScale now work.  xScale is populated with our random number generator.

--- app/src/App.js
+++ app/src/App.js
@@ -13,18 +13,23 @@ const Svg = styled.svg`
 function App() {
   
   const processors = d3.range(5).map(i =>  `${i} CPU` ),
-  random = d3.randomUniform(1000, 50000);
+    random = d3.randomUniform(1000, 50000);
 
   const data = d3.range(1970,2025).map(year => 
     d3.range(5).map(i => ({
-    year: year,
-    name: processors[i],
-    transistors: Math.round(random())
+      year: year,
+      name: processors[i],
+      transistors: Math.round(random())
   })))
 
   return (
     <Svg>
-      <Barchart data={data[0]} x={100} y={50} height={500} />
+      <Barchart 
+        data={data[0]} 
+        x={100} 
+        y={50} 
+        barThickness={20} 
+        width={500} />
     </Svg>
   );
 }
